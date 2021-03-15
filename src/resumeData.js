882184f160843d43cfd0558b524be7fd799c4652
Trevor Pino <trevorpino@yahoo.com>
2021-03-15 16:45:16 -0600

    fist push

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://tpino92.github.io/proffesional-portfolio/",
+    "name": "Trevor Pino",
+    "role": "Full Stack Web Developer and Tech Wizard",
+    "linkedinId":"Your LinkedIn Id",
+    "facebookid": "Your facebookid",
+    "roleDescription": "Skilled in HTML, CSS, JavaScript. Practiced in meeting deadlines, creativity, and teamwork. Excited to develop responsive websites and applications, utilizing different approaches while still collaborating with others to create meaningful experience for the end user.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/trevor-pino-0913971b1/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/TPino92",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/trevor.pino",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"Tech Wizard who recently earned a Certificate in Full Stack Web Development from the University of Arizona. I enjoy dabbling in various parts of frontend development and am always excited to learn about new technologies, write technical articles or simply just playing games with my friends in my free time.",
+    "address":"Albuquerque, NM",
+    "website":"trevorpino@yahoo.com",
+    "education":[
+      {
+        "UniversityName":"University of Arizona",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2021",
+        "Achievements":"Applied aspects of UX and agile development in a recent project where I collaborated in a team of four to develop a single-page MERN app that helps local business owners track their sales, inventory, and attendance at specialty events. Positioned to provide unique perspectives on how end users interact with websites and software platforms by leveraging background in customer service, helping customers and users get their best experience."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Starbucks",
+        "specialization":"Shift Manager",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"2021",
+        "Achievements":"Providing exceptional customer service while maintaining top efficiency within a fastpaced team setting. Training and mentoring new employees. Leading in-store trainings for new promotions."
+      },
+      {
+        "CompanyName":"First American Home Warranty",
+        "specialization":"Inbound Sales Representative",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2015",
+        "Achievements":"Interacting professionally with customers. Consultative sales approach. Extensive customer service and de-escalation of customer complaints."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Run Buddy",
+        "description":"A fitness website. Users can find trainers and fitness package that best works for them ",
+        "imgurl":"images/portfolio/project1.png",
+        "url":"https://tpino92.github.io/run-buddy/"
+      },
+      {
+        "name":"Horizeon Solutions",
+        "description":"Social Media Marketing organization that helps users determine which platforms are suited to their brand, using analytics to find the right markets and increase their lead generation.",
+        "imgurl":"images/portfolio/project2.png", 
+        "url":"https://tpino92.github.io/Horiseon-Solutions/"
+      },
+      {
+        "name":"Private Reserve",
+        "description":"A full-stack MERN application that allows users to view products and add to a shopping cart. Users can also submit reviews on individual products. Users must login to be able to put products into a shopping cart and submit reviews.",  
+        "imgurl":"images/portfolio/project3.png",
+        "url":"https://stormy-thicket-95921.herokuapp.com/"
+      },
+      {
+        "name":"Food Festival",
+        "description":"Website dedicated to finding all things Food Festival. Where they are located, ticket  prices, special events, etc. Users can create an account to store all this information.",
+        "imgurl":"images/portfolio/project4.png",
+        "url":"https://tpino92.github.io/food-festival/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Everything looks pretty much perfect, keep up the excellent work!",
+        "name":"Central Grader"
+      },
+      {
+        "description":"Thank God you figured out the onClick handler!",
+        "name":"Brandi Curley"
+      }
+    ]
+  }
+  
+  export default resumeData
